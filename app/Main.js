27a89c7c8ee5cc6ce0bae1c286f534e79d2b423c
fujian <fujian_en@126.com>
2019-04-20 19:14:55 -0400

    step 5

--- app/Main.js
+++ app/Main.js
@@ -8,15 +8,33 @@ import {
 import { LinearGradient } from 'expo';
 import { primaryGradientArray } from './utils/Colors';
 import Header from './components/Header';
+import Input from './components/Input';
+import SubTitle from './components/SubTitle';
+
 const headerTitle = 'Jiango To Do';
 
 export default class Main extends React.Component {
+  state = {
+    inputValue: ''
+  };
+
+  newInputValue = value => {
+    this.setState({
+      inputValue: value
+    });
+  };
+
   render() {
+    const { inputValue } = this.state;
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
          <StatusBar barStyle="light-content" />
          <View style={styles.centered}>
           <Header title={headerTitle} />
+         </View>
+         <View style={styles.inputContainer}>
+          <SubTitle subtitle={"What's next?"} />
+          <Input inputValue={inputValue} onChangeText={this.newInputValue} />
          </View>
        </LinearGradient>
     );
@@ -29,5 +47,9 @@ const styles = StyleSheet.create({
 	},
   centered: {
     alignItems: 'center',
-  }
+  },
+  inputContainer: {
+    marginTop: 40,
+    paddingLeft: 15
+  },
 });
--- app/utils/Colors.js
+++ app/utils/Colors.js
@@ -1,3 +1,5 @@
 const primaryStart = '#f18a69';
 const primaryEnd = '#d13e60';
 export const primaryGradientArray = [primaryStart, primaryEnd];
+export const inputPlaceholder = '#f1a895';
+export const lighterWhite = '#f4e4e2';

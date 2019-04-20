80549d5d3561cd50d9cbee6f4f4e39f59d855179
fujian <fujian_en@126.com>
2019-04-20 18:33:47 -0400

    step 4

--- app/Main.js
+++ app/Main.js
@@ -7,13 +7,17 @@ import {
 } from 'react-native';
 import { LinearGradient } from 'expo';
 import { primaryGradientArray } from './utils/Colors';
+import Header from './components/Header';
+const headerTitle = 'Jiango To Do';
 
 export default class Main extends React.Component {
   render() {
     return (
       <LinearGradient colors={primaryGradientArray} style={styles.container}>
          <StatusBar barStyle="light-content" />
-         <Text>Jiango is awesome!</Text>
+         <View style={styles.centered}>
+          <Header title={headerTitle} />
+         </View>
        </LinearGradient>
     );
   }
@@ -22,7 +26,8 @@ export default class Main extends React.Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-    justifyContent: 'center',
+	},
+  centered: {
     alignItems: 'center',
-	}
+  }
 });
